6ef1085b950b793685b56724f18e062f5a8c23cf
bkhash <akobuma271@gmail.com>
2025-07-09 12:33:40 +0900

    rowman.rbを修正

--- Formula/rowman.rb
+++ Formula/rowman.rb
@@ -2,7 +2,7 @@ class Rowman < Formula
   desc "A CLI tool to process and filter CSV data"
   homepage "https://github.com/kobayashi-hash/rowman"
   url "https://github.com/kobayashi-hash/rowman/releases/download/v1.0.0/rowman_darwin_arm64.tar.gz"
-  sha256 "cff5f92681ef003974ed947972c2c512c06c786df7f5106d87f1c8df792c030a"
+  sha256 "aebe7a664ba265daa6856e4b252fddb1ae7e32a70d6c0113d36e9f9596ba1803"
   version "1.0.0"
   license "MIT"
   option "without-completions", "Disable bash completions"
